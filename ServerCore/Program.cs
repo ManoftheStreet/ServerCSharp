7c9fff15fbb1b598e9304dd2f388eb6430d5f9e3
ManoftheStreet <zhijian6527@naver.com>
2023-10-04 21:40:24 +0900

    1005학교

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        1004학교
+    }
+}
