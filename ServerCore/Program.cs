4b5d5f79e4c10062562447509554ef45b9e5db29
ManoftheStreet <zhijian6527@naver.com>
2023-10-05 21:38:03 +0900

    SpinLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,8 +4,65 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SpinLock
+    {
+        volatile int _locked = 0;
+
+        public void Acquird()
+        {
+            while(true)
+            {
+                int orginal = Interlocked.Exchange(ref _locked, 1);
+                if (orginal == 0)
+                {
+                    break;
+                }
+            }
+            
+        }
+
+        public void Release()
+        {
+            _locked = 0;
+        }
+
+    }
+
     class Program
     {
-        1004학교
+        static int _num = 0;
+        static SpinLock _lock = new SpinLock();
+
+        static void Thread1()
+        {
+            for(int i = 0; i < 100000; i++)
+            {
+                _lock.Acquird();
+                _num++;
+                _lock.Release();
+            }
+        }
+
+        static void Thread2()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                _lock.Acquird();
+                _num--;
+                _lock.Release();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(Thread1);
+            Task t2 = new Task(Thread2);
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1,t2);
+            Console.WriteLine(_num);
+        }
+
     }
 }
